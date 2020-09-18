5d92b341a738ce4e333eda9a79cfb9630f830d2c
syaifulfahmi <52196122+syaifulfahmi@users.noreply.github.com>
2020-09-18 10:50:07 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,14 @@
+var nim = 123456;
+var nama = "Syaiful";
+var statusMenikah = true;
+var kosong = "ada";
+kosong = null;
+var tidakTerdefinisi;
+var pekerjaan = ["Pengusaha", "Perawat", "Dokter"];
+
+console.log(nim);
+console.log(nama);
+console.log(statusMenikah);
+console.log(kosong);
+console.log(tidakTerdefinisi);	
+console.log(pekerjaan[0]);
